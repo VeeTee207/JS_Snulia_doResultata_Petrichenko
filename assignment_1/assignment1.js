3ef09286938a09f26d1d96a8bad382e1f092a48c
vterehov207 <v.terehov207@gmail.com>
2019-11-30 18:11:34 -0500

    #3. Slightly apdate assignment.js

--- assignment_1/assignment1.js
+++ assignment_1/assignment1.js
@@ -1,22 +1,22 @@
 "use strict";
 // ES6 Example of how to fill created nested object and how extract its values !!
 
-let money = +prompt("How much do you make per month?", ""); // convert to number
+let time = prompt("Enter please todays date in format YYYY-MM-DD:", ""),
+    money = +prompt("How much do you make per month?", ""); // convert to number
 
-let time = prompt("Enter please todays date in format YYYY-MM-DD:", "");
-let appData = { monthlyBudget:money,
+let appData = { monthlyBudget:money,    
                 timeData:time, 
                 expenses:{},        //obligatory payments
                 optionalExpences:{},
                 extraIncome:[],
                 savings:false, };  
 
-let expencesType = prompt("Enter type of your expence in the month ... ", "");
-let expenceAmount = +prompt("How much do you have to spend? ", "0");  //prompt amount and convert it to number 
+let expencesType = prompt("Enter type of your expence in the month ... ", ""),
+    expenceAmount = +prompt("How much do you have to spend? ", "0");  //prompt amount and convert it to number 
 
 appData.optionalExpences[expencesType]=expenceAmount;
-let moneysAfterExpenceses = appData.monthlyBudget - appData.optionalExpences[expencesType];
-let alertPrompt = "So, U can spend up to " + moneysAfterExpenceses/30 + "per day." ;
+let moneysAfterExpenceses = appData.monthlyBudget - appData.optionalExpences[expencesType],
+    alertPrompt = "So, U can spend up to " + moneysAfterExpenceses/30 + "per day." ;
 
 console.log(appData.optionalExpences);  
 // console.log(appData.optionalExpences[expencesType]); 
