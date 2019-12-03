05de89e8b3eef4fb443f6b2a7e264a09251b1206
vterehov207 <v.terehov207@gmail.com>
2019-12-02 20:20:35 -0500

    #5 add functions to appData object, modified chooseIncome

---
+++ assignment_1/s2.js
@@ -0,0 +1,12 @@
+"use strict";
+
+alert("hello s2")
+let a = "1212",
+    b = '56';
+
+console.log(typeof(a) + ' ' + typeof(b));
+//  if (s === "") { console.log("hello s2"); }
+if ( typeof(a)==='string' && typeof(a) != null && a != "" && a.length < 50) {
+    ( typeof(a)==='string' && typeof(a) != null && typeof(b) != null && a != "" && b != "" && a.length < 50)
+    console.log ("done");
+} else { console.log ("wrong data "); }
